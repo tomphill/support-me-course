c07f2c188c3f195aa08f12a88a552b85ad3bcd21
tom <tomphill@live.co.uk>
2024-02-20 22:38:35 +0800

    Update dependencies and add new components

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -3,11 +3,14 @@ import MenuItem from "./menu-item";
 import MenuTitle from "./menu-title";
 import Link from "next/link";
 import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
+import { cn } from "@/lib/utils";
 
-export default function MainMenu() {
+export default function MainMenu({ className }: { className?: string }) {
   return (
-    <nav className="bg-muted overflow-auto p-4 flex flex-col">
-      <header className="border-b dark:border-b-black border-b-zinc-300 pb-4">
+    <nav
+      className={cn(`md:bg-muted overflow-auto p-4 flex flex-col`, className)}
+    >
+      <header className="hidden md:block border-b dark:border-b-black border-b-zinc-300 pb-4">
         <MenuTitle />
       </header>
       <ul className="py-4 grow">
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,35 @@
+"use client";
+
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import MainMenu from "./components/main-menu";
+import MenuTitle from "./components/menu-title";
+import { MenuIcon } from "lucide-react";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
   return (
-    <div className="grid grid-cols-[250px_1fr] h-screen">
-      <MainMenu />
+    <div className="md:grid md:grid-cols-[250px_1fr] h-screen">
+      {isDesktop ? (
+        <MainMenu />
+      ) : (
+        <div className="p-4 flex justify-between sticky top-0 left-0 bg-background border-b border-border">
+          <MenuTitle />
+          <Drawer direction="right">
+            <DrawerTrigger>
+              <MenuIcon />
+            </DrawerTrigger>
+            <DrawerContent>
+              <MainMenu />
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome back, Tom!</h1>
         {children}
